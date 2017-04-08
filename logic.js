cbabc4ea9c93e7d0a25a49e8a6a296abccfb2265
chrisxjacobi <chrisxjacobi@gmail.com>
2017-04-08 10:37:32 -0500

    reverting

--- logic.js
+++ logic.js
@@ -1,42 +1,100 @@
 var wrestlers = ["Yokozuna", "Vader", "Raven", "Mabel",
     "Kamala", "Tatanka", "Sting", "Goldust",
-    "Tazz", "Goldberg", "Kane", "Diesel"];
+    "Tazz", "Goldberg", "Kane", "Diesel"
+];
+
+
 
 var wins = 0;
 var losses = 0;
 var turns = 9;
 
+var currentPick = "";
+var lettersInPick = [];
+var currentMatch = [];
+var wrongGuesses = [];
+
 
 function play() {
-	turns = 9;
+    turns = 9;
 
-	currentPick = wrestlers[Math.floor(Math.random() * wrestlers.length)];
-	lettersInPick = currentPick.split("");
-	blanks = lettersInPick.length;
+    currentPick = wrestlers[Math.floor(Math.random() * wrestlers.length)];
+    lettersInPick = currentPick.split("");
+    blanks = lettersInPick.length;
 
-	console.log(currentPick);
+    console.log(currentPick);
 
-	currentMatch = [];
-	wrongGuesses = [];
+    currentMatch = [];
+    wrongGuesses = [];
 
-	for (var i = 0; i < blanks; i++) {
-		currentMatch.push("_");
-	}
+    for (var i = 0; i < blanks; i++) {
+        currentMatch.push("_");
+    }
 
-	console.log(currentMatch);
+    console.log(currentMatch);
 
-	document.getElementById("kickouts").innerHTML = "Kickouts remaining: " + turns;
+    document.getElementById("kickouts").innerHTML = "Kickouts remaining: " + turns;
 
-	document.getElementById("wordBlanks").innerHTML = "Current Wrestler: " + currentMatch.join(" ");
+    document.getElementById("wordBlanks").innerHTML = "Current Wrestler: " + currentMatch.join(" ");
 
-	document.getElementById("lettersGuessed").innerHTML = "Letters Guessed: " + wrongGuesses.join(" ");
-
-
+    document.getElementById("lettersGuessed").innerHTML = "Letters Guessed: " + wrongGuesses.join(" ");
 
 }
+
+function checkLetter(letter) {
+    var correctLetter = false;
+
+    for (var i = 0; i < blanks; i++) {
+        if (currentPick[i] == letter) {
+            correctLetter = true;
+        }
+    }
+
+    if (correctLetter) {
+        for (var i = 0; i < blanks; i++) {
+            if (currentPick[i] == letter) {
+                currentMatch[i] = letter;
+            }
+        }
+
+        console.log(currentMatch);
+    } else {
+        wrongGuesses.push(letter);
+        turns--;
+    }
+}
+
+function matchDone() {
+    console.log("win count: " + wins + " Loss Count: " + losses + " guesses left: " + turns);
+
+    document.getElementById("kickouts").innerHTML = "Kickouts remaining: " + turns;
+
+    document.getElementById("wordBlanks").innerHTML = "Current Wrestler: " + currentMatch.join(" ");
+
+    document.getElementById("lettersGuessed").innerHTML = "Letters Guessed: " + wrongGuesses.join(" ");
+
+    if (lettersInPick.toString() == currentMatch.toString()) {
+        wins++;
+        alert("You're the champion!");
+
+        document.getElementById("wins").innerHTML = wins;
+        play();
+    } else if (turns == 0) {
+        losses++;
+        alert("You've been pinned!");
+
+        document.getElementById("losses").innerHTML = losses;
+        play();
+    }
+
+}
+
+play();
+
 
 document.onkeyup = function() {
-	letterChosen = String.fromCharCode(event.keyCode).toLowerCase();
-}
+    letterChosen = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetter(letterChosen);
 
-play()
+    matchDone();
+}
